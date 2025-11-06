f1a864dccacaa215226782158b152aeede7e64c9
mahmed <mahmed030888@gmail.com>
2025-11-06 17:29:39 +0500

    Basic structure of AnalyticsFeature

---
+++ components/Home/AnalyticsFeature/AnalyticsFeature.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const AnalyticsFeature = () => {
+  return (
+    <div className='pt-24 pb-16'>
+        {/* Define Grid  */}
+        <div className='w-[95%] sm:w-[80%] mx-auto items-center grid-cols-1 lg:grid-cols-2 gap-10'>
+            {/* Image Content   */}
+            <div>
+                {/* To Be Continueed...  At video 1:15:25 */}
+            </div>
+            {/* Text  Content  */}
+            <div></div>
+
+        </div>
+    </div>
+  )
+}
+
+export default AnalyticsFeature
