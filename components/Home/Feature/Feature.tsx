b1353029cc01f0979d33d17ed4a4db927fc770d0
mahmed <mahmed030888@gmail.com>
2025-11-08 15:03:03 +0500

    feature section completed

--- components/Home/Feature/Feature.tsx
+++ components/Home/Feature/Feature.tsx
@@ -75,7 +75,7 @@
 
 // export default Feature;
 }
-//   to be continued... at video 1:35:07
+
 import React from "react";
 import {
   FaBook,
@@ -89,7 +89,7 @@ import {
   FaShieldAlt,
 } from "react-icons/fa";
 
-const Feature = () => {
+const Feature = ()=> {
   const features = [
     { icon: <FaLayerGroup className="text-red-500 text-2xl" />, text: "50+ Unique Design Blocks" },
     { icon: <FaLaptop className="text-blue-500 text-2xl" />, text: "Multiple Layouts" },
@@ -133,17 +133,18 @@ const Feature = () => {
       </h1>
 
       <div className="grid mt-16 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-        {features.map((feature, index) => (
+        {features.map((feature,index) => (
           <div
             key={index}
             className="flex items-center justify-center p-4 bg-white rounded-lg shadow-md space-x-3 hover:shadow-lg transition duration-300"
-          >
+            >
             <div className="text-3xl w-14 h-14 bg-gray-100 flex items-center justify-center rounded-full">
               <span>{feature.icon}</span>
             </div>
             <span className="font-semibold text-gray-800">{feature.text}</span>
           </div>
-        ))}
+          ))
+        }
         </div>
         </div>
       </div>
