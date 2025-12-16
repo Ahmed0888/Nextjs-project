6c977b9a9100cd87d8f5cdb9849523beb85398b0
M.Ahmed <mahmed030888@gmail.com>
2025-12-16 15:34:43 +0500

    Integrate Google Tag Manager in ResponsiveNav
    Added Google Tag Manager scripts for tracking.

--- components/Home/Navbar/ResponsiveNav.tsx
+++ components/Home/Navbar/ResponsiveNav.tsx
@@ -48,6 +48,16 @@ const ResponsiveNav = () => {
     };
   }, []);
 
+  <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-4YJML9QWGP"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-4YJML9QWGP');
+</script>
+
   return (
     <>
       <Nav openNav={handleNavShow} />
